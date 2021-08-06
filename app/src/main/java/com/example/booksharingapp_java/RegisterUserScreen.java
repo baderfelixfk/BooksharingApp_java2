c4cc8585132ea83b9938abff3ace5a0ba74b167d
Felix <bader.felix@outlook.com>
2021-08-06 23:59:35 +0200

    First Commit

--- app/src/main/java/com/example/booksharingapp_java/RegisterUserScreen.java
+++ app/src/main/java/com/example/booksharingapp_java/RegisterUserScreen.java
@@ -2,16 +2,21 @@ package com.example.booksharingapp_java;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUserScreen extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,14 +34,17 @@ public class RegisterUserScreen extends AppCompatActivity implements View.OnClic
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE|WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN|WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
+
+
         mAuth = FirebaseAuth.getInstance();
+
+
+
 
         banner = (TextView) findViewById(R.id.txt_MainAppName2);
         banner.setOnClickListener(this);
-
         btn_registeruser = (Button) findViewById(R.id.btn_register);
         btn_registeruser.setOnClickListener(this);
-
         et_Name = (EditText)  findViewById(R.id.etxt_FullNameRegister);
         et_Age = (EditText)  findViewById(R.id.etxt_AgeRegister);
         et_Email = (EditText)  findViewById(R.id.etxt_emailAddressRegister);
@@ -90,13 +98,7 @@ public class RegisterUserScreen extends AppCompatActivity implements View.OnClic
         }
 
         // Check for valid email
-        if(Patterns.EMAIL_ADDRESS.matcher(email).matches())
-        {
-            et_Email.setError("Please enter valid Email");
-            et_Email.requestFocus();
-            return;
 
-        }
         // Check for password a bit
         if(password.length() < 6)
         {
@@ -104,6 +106,34 @@ public class RegisterUserScreen extends AppCompatActivity implements View.OnClic
             et_password.requestFocus();
             return;
         }
+
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                User user = new User(name, age, email);
+
+                                FirebaseDatabase.getInstance().getReference("users")
+                                        .child((FirebaseAuth.getInstance().getCurrentUser().getUid()))
+                                        .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(RegisterUserScreen.this, "User han been registrated", Toast.LENGTH_LONG).show();
+                                        } else {
+                                            Toast.makeText(RegisterUserScreen.this, "User registration failed", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+
+                            }
+                        }
+                    });
+
 
     }
 
---
+++ app/src/main/java/com/example/booksharingapp_java/User.java
@@ -0,0 +1,19 @@
+package com.example.booksharingapp_java;
+
+public class User {
+
+    public String name, age, email;
+     public User()
+     {
+
+     }
+
+     public User( String name, String age, String email)
+     {
+         this.age = age;
+         this.name = name;
+         this.email = email;
+
+     }
+
+}
