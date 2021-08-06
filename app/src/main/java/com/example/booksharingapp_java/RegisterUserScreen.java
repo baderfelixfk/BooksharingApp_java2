6a56a1ad370c9082d6f02301d2c46fa60ea8b76b
Felix <bader.felix@outlook.com>
2021-08-06 19:11:31 +0200

    First Commit

--- app/src/main/java/com/example/booksharingapp_java/RegisterUserScreen.java
+++ app/src/main/java/com/example/booksharingapp_java/RegisterUserScreen.java
@@ -11,5 +11,6 @@ public class RegisterUserScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user_screen);
 
+
     }
 }
