9a2bf4da8058dfece7c67d6b2087fb87ae3a2151
Felix <bader.felix@outlook.com>
2021-08-07 19:15:01 +0200

    First Commit

--- app/src/main/java/com/example/booksharingapp_java/MainActivity.java
+++ app/src/main/java/com/example/booksharingapp_java/MainActivity.java
@@ -4,13 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView register;
+    private TextView textViewRegister;
+    private Button buttonSignin;
+    private EditText editTextEmail, editTextPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +31,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-        register = (TextView) findViewById(R.id.txt_registerMain);
-        register.setOnClickListener(this);
+        textViewRegister = (TextView) findViewById(R.id.txt_registerMain);
+        textViewRegister.setOnClickListener(this);
+
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE|WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         //THis is a little workaround to re initialize the keyboard for the correct background
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN|WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
+        buttonSignin  = (Button) findViewById(R.id.btn_login);
+        buttonSignin.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.etxt_EmailAddress);
+        editTextPassword = (EditText)  findViewById(R.id.etxt_Password);
 
     }
 
+    // This function checks for a valid email address, true or false
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
 
     @Override
     public void onClick(View view) {
@@ -36,8 +70,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.txt_registerMain:
                 startActivity(new Intent(this,RegisterUserScreen.class));
                 break;
+            case R.id.btn_login:
+                userLogin();
+                break;
 
         }
+
+    }
+
+
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            editTextEmail.setError("Please enter a email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!isValidEmailAddress(email))
+        {
+            editTextEmail.setError("Please enter valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.length()<6)
+        {
+            editTextPassword.setError("Password must be at least 6 chars");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.isEmpty())
+        {
+            editTextPassword.setError("Please enter a password");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful())
+                {
+                    // redirect to user prfilel
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Failed to login", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/booksharingapp_java/RegisterUserScreen.java
+++ app/src/main/java/com/example/booksharingapp_java/RegisterUserScreen.java
@@ -54,6 +54,12 @@ public class RegisterUserScreen extends AppCompatActivity implements View.OnClic
 
     }
 
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
 
     @Override
     public void onClick(View view) {
@@ -81,13 +87,7 @@ public class RegisterUserScreen extends AppCompatActivity implements View.OnClic
         }
     }
 
-    // This function checks for a valid email address, true or false
-    public boolean isValidEmailAddress(String email) {
-        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-        java.util.regex.Matcher m = p.matcher(email);
-        return m.matches();
-    }
+
 
 
     // This functions registers a new user
